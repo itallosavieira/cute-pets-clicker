040052cff6e2eb7b6c2e039c08302cfcd70b9254
itallosavieira <itallo.sa.vieira@gmail.com>
2020-07-29 18:14:45 -0300

    add two posts

--- js/script.js
+++ js/script.js
@@ -1,10 +1,20 @@
-const imgPets = document.querySelector(".pets-img");
-const clickPets = document.querySelector(".pets-clicks");
-let counterClicks = 1;
+let imgPets = document.querySelectorAll(".pets-img");
+let clickPets = document.querySelectorAll(".pets-clicks");
 
-const getClick = function() {    
-    clickPets.innerHTML = `${counterClicks}`;
-    counterClicks += 1;
+let counterClicks1 = 1;
+let counterClicks2 = 1;
+
+const getClick1 = function() {
+    clickPets[0].innerHTML = `${counterClicks1}`;
+    counterClicks1 += 1;
 };
 
-imgPets.addEventListener('click', getClick);
+const getClick2 = function() {
+    clickPets[1].innerHTML = `${counterClicks2}`;
+    counterClicks2 += 1; 
+};
+
+imgPets[0].addEventListener('click', getClick1);
+imgPets[1].addEventListener('click', getClick2);
+
+
